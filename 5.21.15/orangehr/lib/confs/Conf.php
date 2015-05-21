b7aee68b937560cfbca8d88297219f846bb9a8d6
bellealejandria <rizabelle.alejandria@gmail.com>
2015-05-21 13:20:02 +0800

    login

---
+++ 5.21.15/orangehr/lib/confs/Conf.php
@@ -0,0 +1,28 @@
+<?php
+class Conf {
+
+	var $smtphost;
+	var $dbhost;
+	var $dbport;
+	var $dbname;
+	var $dbuser;
+	var $version;
+
+	function Conf() {
+
+		$this->dbhost	= 'localhost';
+		$this->dbport 	= '3306';
+		if(defined('ENVIRNOMENT') && ENVIRNOMENT == 'test'){
+		$this->dbname    = 'test_orangehr';		
+		}else {
+		$this->dbname    = 'orangehr';
+		}
+		$this->dbuser    = 'root';
+		$this->dbpass	= 'password';
+		$this->version = '3.3';
+
+		$this->emailConfiguration = dirname(__FILE__).'/mailConf.php';
+		$this->errorLog =  realpath(dirname(__FILE__).'/../logs/').'/';
+	}
+}
+?>
---
+++ 5.21.15/orangehr/symfony/apps/orangehrm/templates/noHeader.php
@@ -0,0 +1,16 @@
+<?php require_once '_header.php'; ?>
+
+    </head>
+    <body>
+      
+        <div id="wrapper">
+            
+            <div id="login">
+
+                  <?php echo $sf_content ?>
+
+            </div> <!-- content -->
+          
+        </div> <!-- wrapper -->
+        
+<?php require_once '_footer.php'; ?>
